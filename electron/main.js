6a056b73a79b1b79ee79d761215239c4ea4667a9
a1098990276-hue <a1098990276@gmail.com>
2026-04-02 14:30:07 +0300

    Update electron/main.js content

--- electron/main.js
+++ electron/main.js
@@ -1,24 +1,31 @@
-// Main Electron file
-
+const path = require('path');
 const { app, BrowserWindow } = require('electron');
 
+const VITE_DEV_URL = process.env.VITE_DEV_URL || 'http://localhost:5173';
+
 function createWindow() {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            preload: path.join(__dirname, 'preload.js'),
-        }
-    });
-    win.loadURL('http://localhost:3000');
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  });
+
+  win.loadURL(VITE_DEV_URL);
+
+  if (!app.isPackaged) {
+    win.webContents.openDevTools({ mode: 'detach' });
+  }
 }
 
-app.whenReady().then(createWindow);
-
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit();
+app.whenReady().then(() => {
+  createWindow();
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
 });
 
-app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
 });
