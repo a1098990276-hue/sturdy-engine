bfc7134fec08e8ff8c20c07f62d41f8befed008d
a1098990276-hue <a1098990276@gmail.com>
2026-04-02 14:37:41 +0300

    Add Electron preload script

---
+++ electron/preload.js
@@ -0,0 +1,13 @@
+// Electron preload script
+
+// Expose a method to the renderer process
+window.api = {
+    // Method to load data
+    loadData: async () => {
+        // Implementation here
+    },
+    // Method to save data
+    saveData: async (data) => {
+        // Implementation here
+    }
+};
