68af56c4f885688a930abcb6cdd04431969e60f4
a1098990276-hue <a1098990276@gmail.com>
2026-04-02 14:04:13 +0300

    Add main.js for Electron application

---
+++ electron/main.js
@@ -0,0 +1,24 @@
+// Main Electron file
+
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+        }
+    });
+    win.loadURL('http://localhost:3000');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
