669eb1d7cf1d92d75024d2f5545888bf0d335958
a1098990276-hue <a1098990276@gmail.com>
2026-04-02 05:39:12 +0300

    Update main.jsx file with initial React app setup.

---
+++ desktop-app/src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './styles.css';
+import './i18n';
+
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
